4e9d69cb1f098db4d7ccbdae03655612e704cbda
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-04-01 13:23:58 -0600

    conecte los botones del menu con sus ventanas

--- src/main/java/com/example/banco/BancoController.java
+++ src/main/java/com/example/banco/BancoController.java
@@ -1,8 +1,11 @@
 package com.example.banco;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class BancoController {
 
@@ -10,17 +13,33 @@ public class BancoController {
     private Label mensajeLabel;
 
     @FXML
-    protected void onConsultarSaldo() {
-
+    protected void onConsultarSaldo() throws IOException {
+        Stage stage = (Stage) mensajeLabel.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("consulta.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Consulta de Saldo");
+        stage.show();
     }
 
     @FXML
-    protected void onDepositar() {
-
+    protected void onDepositar() throws IOException {
+        Stage stage = (Stage) mensajeLabel.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("deposito.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Depósito");
+        stage.show();
     }
 
     @FXML
-    protected void onRetirar() {
+    protected void onRetirar() throws IOException {
+        Stage stage = (Stage) mensajeLabel.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("retiro-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Retiro");
+        stage.show();
     }
 
     @FXML
