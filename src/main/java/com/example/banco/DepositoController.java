7eee6f35412701aec2248dee6e96e63c54909bd1
Ciscoboy-josh <Orochiponce1@gmail.com>
2026-04-01 13:24:09 -0600

    arregle el deposito para que use SesionBanco

--- src/main/java/com/example/banco/DepositoController.java
+++ src/main/java/com/example/banco/DepositoController.java
@@ -2,7 +2,7 @@ package com.example.banco;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -22,49 +22,42 @@ public class DepositoController {
     private Button regresar;
 
     @FXML
-    private double cantidadDeposito;
-
-    @FXML
     private Label resultadoDeposito;
 
     @FXML
     private Label saldoLabel;
 
     @FXML
-    protected void deposito (){
-        Stage stage = new Stage();
-        stage.setTitle("Depósito");
+    public void initialize() {
+        saldoLabel.setText(String.format("Saldo actual: $%.2f", SesionBanco.getSaldo()));
+    }
 
-        saldoLabel.setText(saldo);
-
-
-        cantidadDeposito = Double.parseDouble(String.valueOf(this.txtCantidad.getText()));
-
-        if (cantidadDeposito > 0 ){
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("Deposito.fxml"));
-                Parent root = (Parent) loader.load();
-
-                saldo = saldo + cantidadDeposito;
-
-
+    @FXML
+    protected void deposito() {
+        try {
+            double cantidadDeposito = Double.parseDouble(txtCantidad.getText());
+            if (cantidadDeposito > 0) {
+                SesionBanco.depositar(cantidadDeposito);
+                saldoLabel.setText(String.format("Saldo actual: $%.2f", SesionBanco.getSaldo()));
                 resultadoDeposito.setVisible(true);
                 resultadoDeposito.setText("Depósito exitoso");
-
-            } catch (IOException e){
-                e.printStackTrace();
-
+            } else {
                 resultadoDeposito.setVisible(true);
                 resultadoDeposito.setText("Error: Cantidad no válida");
             }
-
-
+        } catch (NumberFormatException e) {
+            resultadoDeposito.setVisible(true);
+            resultadoDeposito.setText("Error: Cantidad no válida");
         }
-
     }
-    @FXML
-    protected void salir (){
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("banco-view.fxml"));
 
+    @FXML
+    protected void salir() throws IOException {
+        Stage stage = (Stage) regresar.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("banco-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("Menú Principal");
+        stage.show();
     }
 }
