6aca37b716c933a76fe1e3d45cef0814631af87f
Andrés Angel Martínez Rojas <AndresAngel62>
2026-03-31 20:35:29 -0600

    realizamos el boton para reglesar al menu y tambien mostramos a pantalla el saldo actual

---
+++ src/main/java/com/example/banco/ConsultaController.java
@@ -0,0 +1,33 @@
+package com.example.banco;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class ConsultaController {
+
+    @FXML
+    private Label lblSaldo;
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    public void initialize() {
+        lblSaldo.setText(String.format("Saldo actual:git", SesionBanco.getSaldo()));
+    }
+
+    @FXML
+    protected void onRegresarClick() throws IOException {
+        Stage stage = (Stage) btnRegresar.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Menú Principal");
+        stage.show();
+    }
+}
